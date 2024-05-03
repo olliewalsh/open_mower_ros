7bcb3958f9c39df87f80fefbea67cf04671ab2c9
Ollie Walsh <ollie.walsh@gmail.com>
2024-05-03 13:56:26 +0100

    Add ESC voltage to mower status

--- src/mower_comms/src/mower_comms.cpp
+++ src/mower_comms/src/mower_comms.cpp
@@ -163,6 +163,7 @@ void convertStatus(xesc_msgs::XescStateStamped &vesc_status, mower_msgs::ESCStat
     }
     ros_esc_status.tacho = vesc_status.state.tacho;
     ros_esc_status.current = vesc_status.state.current_input;
+    ros_esc_status.voltage = vesc_status.state.voltage_input;
     ros_esc_status.temperature_motor = vesc_status.state.temperature_motor;
     ros_esc_status.temperature_pcb = vesc_status.state.temperature_pcb;
 }
