119adf9cb117b333331e9587f4a82753710c1992
Ollie Walsh <ollie.walsh@gmail.com>
2025-05-20 22:41:36 +0100

    Disable lift/tilt emergency while docking but not on approach

--- src/mower_logic/src/mower_logic/behaviors/DockingBehavior.cpp
+++ src/mower_logic/src/mower_logic/behaviors/DockingBehavior.cpp
@@ -329,7 +329,7 @@ bool DockingBehavior::redirect_joystick() {
 }
 
 uint8_t DockingBehavior::get_sub_state() {
-  return 1;
+  return inApproachMode ? 0 : 2;
 }
 uint8_t DockingBehavior::get_state() {
   return mower_msgs::HighLevelStatus::HIGH_LEVEL_STATE_AUTONOMOUS;
